7f80435c6a0f5a5825bf0dae631f62b1a5864b6e
rossyeager <ross@boostedboards.com>
2017-05-13 15:32:29 -0700

    Frist crack at dist tooling

---
+++ python/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+import subprocess
+import sys
+import os
+
+# get the version included in the __init__
+import tools
+
+folders = [
+        os.path.join('tools','images'),
+        ]
+
+fnames = []
+
+def remove_first_dir(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return os.path.join(*allparts[1:])
+
+datafiles_dir = os.path.join(os.path.expanduser('~'), 'datafiles')
+ret = {}
+for folder in folders:
+    for d, _, files in os.walk(folder):
+        ret[os.path.join(datafiles_dir, remove_first_dir(d))] = [os.path.join(d,f) for f in files]
+
+for fname in fnames:
+    print 'add', fname
+    dirname = os.path.dirname(os.path.join(datafiles_dir, remove_first_dir(fname)))
+    if dirname in ret:
+        ret[dirname].append(fname)
+    else:
+        ret[dirname] = [fname]
+
+datafiles = []
+for k, v in ret.iteritems():
+    datafiles.append((k, v))
+
+for d in datafiles:
+    print d
+
+setup(
+    name='cgtools',
+    version='1.0.0',
+    author='Ross Yeager',
+    author_email='ryeager.design@gmail.com',
+    url='https://github.com/rossyeager/camarogeneration',
+    install_requires=[] ,
+    packages=find_packages(),
+    data_files=datafiles,
+    entry_points={
+        'console_scripts': []
+    }
+)
